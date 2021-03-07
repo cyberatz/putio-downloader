8c382d6578148b256ac91d63883fc5c299c43754
Andre <pid@Legolas.local>
2021-03-07 19:19:00 +0100

    update sync

--- sync.sh
+++ sync.sh
@@ -3,12 +3,13 @@
 #rclone sync -P putio:Movies /data/
 rclone sync -P putio: /data/
 
-echo "Check status of downloads"
+#echo "Check status of downloads"
 
-echo "Delete from Putio"
-rclone purge putio:Movies
+#echo "Delete from Putio"
+#rclone purge putio:Movies
+rclone purge putio: /data/
 
-echo "Make Movies Folder"
-rclone mkdir putio:Movies
+#echo "Make Movies Folder"
+#rclone mkdir putio:Movies
 
 sleep 60
