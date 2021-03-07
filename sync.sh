b0ae76115680115deba3d178c74fda7df360dde5
Andre <pid@Legolas.local>
2021-03-07 19:16:44 +0100

    Update sync

--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #rclone sync putio:Movies /data
-rclone sync -P putio:Movies /data/
+#rclone sync -P putio:Movies /data/
+rclone sync -P putio: /data/
 
 echo "Check status of downloads"
 
