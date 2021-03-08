6f22307fae4e3e6fdcbaede4cb7cdf2e004d5264
Andre <pid@Legolas.home>
2021-03-08 11:12:59 +0100

    added putio purge

--- sync.sh
+++ sync.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+echo "Sync downloads"
 #rclone sync putio:Movies /data
 #rclone sync -P putio:Movies /data/
 rclone sync -P putio: /data/
@@ -7,9 +11,15 @@ rclone sync -P putio: /data/
 
 #echo "Delete from Putio"
 #rclone purge putio:Movies
-rclone purge putio: 
+#rclone purge putio:
 
 #echo "Make Movies Folder"
-rclone mkdir putio:Movies
+#rclone mkdir putio:Movies
+
+echo "Delete from Putio"
+#for i in $(rclone lsd putio:  | sed 's/\-1//g' | sed 's/\([0-9][0-9][0-9][0-9]\)\-\([0-9][0-9]\)\-\([0-9][0-9]\)//g' | sed 's/\([0-9][0-9]\)\:\([0-9][0-9]\)\:\([0-9][0-9]\)//g' | sed 's/\ \ \ //g' | awk -F'  ' '{print $1}') ;do echo "$i";done
+for i in $(rclone lsd putio:  | sed 's/\-1//g' | sed 's/\([0-9][0-9][0-9][0-9]\)\-\([0-9][0-9]\)\-\([0-9][0-9]\)//g' | sed 's/\([0-9][0-9]\)\:\([0-9][0-9]\)\:\([0-9][0-9]\)//g' | sed 's/\ \ \ //g' | awk -F'  ' '{print $1}') ;do rclone purge putio:"$i";done
+
+IFS=$SAVEIFS
 
 sleep 60
