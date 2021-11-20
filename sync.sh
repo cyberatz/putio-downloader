943b65301c1f33cde569789d013ea84731211c2b
Andre van Schalkwyk <andre@hebe.ch>
2021-11-20 16:58:32 +0100

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,26 +4,13 @@ IFS=$(echo -en "\n\b")
 mkdir -p /data/temp /data/data
 
 echo "Sync downloads"
-#rclone sync putio:Movies /data
-#rclone sync -P putio:Movies /data/
-rclone sync -P putio: /data/temp/
+#rclone sync -P putio: /data/temp/
+rclone move -P --delete-empty-src-dirs putio: /data/temp/ 
 mv /data/temp/* /data/data/ 
 
-#echo "Check status of downloads"
-
 #echo "Delete from Putio"
-#rclone purge putio:Movies
-#rclone purge putio:
-
-#echo "Make Movies Folder"
-#rclone mkdir putio:Movies
-
-echo "Delete from Putio"
-#for i in $(rclone lsd putio:  | sed 's/\-1//g' | sed 's/\([0-9][0-9][0-9][0-9]\)\-\([0-9][0-9]\)\-\([0-9][0-9]\)//g' | sed 's/\([0-9][0-9]\)\:\([0-9][0-9]\)\:\([0-9][0-9]\)//g' | sed 's/\ \ \ //g' | awk -F'  ' '{print $1}') ;do echo "$i";done
-#for i in $(rclone lsd putio:  | sed 's/\-1//g' | sed 's/\([0-9][0-9][0-9][0-9]\)\-\([0-9][0-9]\)\-\([0-9][0-9]\)//g' | sed 's/\([0-9][0-9]\)\:\([0-9][0-9]\)\:\([0-9][0-9]\)//g' | sed 's/\ \ \ //g' | awk -F'  ' '{print $1}') ;do rclone purge putio:"$i";done
-for i in $(rclone lsd putio:   | sed 's/\([0-9][0-9][0-9][0-9]\)\-\([0-9][0-9]\)\-\([0-9][0-9]\)//g' | sed 's/\([0-9][0-9]\)\:\([0-9][0-9]\)\:\([0-9][0-9]\)//g' | sed 's/-1//g' | sed 's/ \+ //g') ;do rclone purge putio:"$i";done
-#for i in $(rclone ls putio: | sed 's/[[:digit:]]\+\ //g') ;do rclone deletefile putio:"$i";done
-rclone delete putio:
+#for i in $(rclone lsd putio:   | sed 's/\([0-9][0-9][0-9][0-9]\)\-\([0-9][0-9]\)\-\([0-9][0-9]\)//g' | sed 's/\([0-9][0-9]\)\:\([0-9][0-9]\)\:\([0-9][0-9]\)//g' | sed 's/-1//g' | sed 's/ \+ //g') ;do rclone purge putio:"$i";done
+#rclone delete putio:
 
 IFS=$SAVEIFS
 
